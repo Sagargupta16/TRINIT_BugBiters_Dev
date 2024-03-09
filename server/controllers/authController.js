aca1c8349ee06d883b0dc92efdcb4c0bf101a04f
Sagar Gupta <sg85207@gmail.com>
2024-03-09 09:57:45 +0530

    Add validation for tutor role

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,10 +24,18 @@ exports.postSignup = async (req, res) => {
 			});
 		}
 
+		if(user.role !== 'tutor' && user.role !== 'student') return res.status(400).json({ errors: ['Invalid role'] });
+
 		const hashedPassword = await bcrypt.hash(user.password, Number(process.env.JWT_SALT_ROUNDS));
 		user.password = hashedPassword;
 
 		if (user.role === 'tutor') {
+			if (user.language.length === 0) {
+				return res.status(400).json({ errors: ['Atleast 1 language required'] });
+			}
+			if (user.yearsOfExperience < 0) {
+				return res.status(400).json({ errors: ['Years of experience must be greater than 0'] });
+			}
 			await new Tutor(user).save();
 		} else {
 			await new Student(user).save();
